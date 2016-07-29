ba769512794fe2e0b6b2091bf030e011bcae4514
Junji Hashimoto <junji.hashimoto@gree.net>
2016-07-29 18:13:08 +0900

    Add install

--- test.sh
+++ test.sh
@@ -5,15 +5,15 @@ if [ ! -d data ] ;then
 fi
 /usr/bin/flarei --data-dir=data --server-name=localhost --server-port=20001 --monitor-interval=1 --monitor-threshold=2 &
 sleep 3
+sudo grep flare /var/log/syslog 
 sudo netstat -anp | grep flare
 /usr/bin/flared --data-dir=data --index-server-name=localhost --index-server-port=20001 --server-name=localhost --server-port=20002 &
 sleep 3
+sudo grep flare /var/log/syslog 
 sudo netstat -anp | grep flare
-$ echo stats nodes| nc localhost 20001
-STAT localhost:20002:role proxy
-STAT localhost:20002:state active
-STAT localhost:20002:partition -1
-STAT localhost:20002:balance 0
-STAT localhost:20002:thread_type 16
-END
-
+echo stats nodes | nc localhost 20001
+echo -e "role localhost 20002 master 1 0\nquit" | nc localhost 20001
+sleep 3
+sudo grep flare /var/log/syslog 
+sudo netstat -anp | grep flare
+echo stats nodes | nc localhost 20001
